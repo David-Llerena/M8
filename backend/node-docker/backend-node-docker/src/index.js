d0ad4d48bdc7293866efc6f9074248cbfb536fd0
David Andres Llerena Amon <dllerena96@outlook.com>
2025-09-30 15:55:53 -0500

    Se agrega el .env.test

--- backend/node-docker/backend-node-docker/src/index.js
+++ backend/node-docker/backend-node-docker/src/index.js
@@ -1,13 +1,18 @@
+require('dotenv').config();
+
 const express = require('express');
 const app = express();
+const port = process.env.PORT || 3000;
+const jwtsecret = process.env.JWT_SECRET
 
 app.use(express.json());
+console.log('JWT Secret:', jwtsecret); // Verifica que la variable de entorno se haya cargado correctamente
+console.log('Port:', port); // Verifica que la variable de entorno se haya cargado correctamente
 
 app.get('/', (req, res) => {
   res.send('API backend funcionando');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+app.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
 });
